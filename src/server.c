ec36a9a36aa3c42d3de077b4c2ca52b9d74708ab
Leon Júnio Martins Ferreira <juniohuy2gamer@gmail.com>
2023-10-16 00:15:21 -0300

    Server fixed

--- src/server.c
+++ src/server.c
@@ -11,22 +11,27 @@
 #define MAX_CLIENTS 10 // Adjust the maximum number of clients as needed
 #define BUF_SIZE 4096
 
-void handleClient(int client, int clients[], int *client_sz) {
+void handle_client(int client, int clients[], int *client_sz)
+{
     char buff[BUF_SIZE];
     ssize_t bytesReceived;
 
     bytesReceived = recv(client, buff, sizeof(buff), 0);
-    if (bytesReceived == -1) {
+    if (bytesReceived == -1)
+    {
         perror("recv");
         close(client);
         return;
     }
 
     // Broadcast the received message to all clients
-    for (int i = 0; i < *client_sz; i++) {
+    for (int i = 0; i < *client_sz; i++)
+    {
         int c = clients[i];
-        if (c != client) {
-            if (send(c, buff, bytesReceived, 0) == -1) {
+        if (c != client)
+        {
+            if (send(c, buff, bytesReceived, 0) == -1)
+            {
                 perror("send");
             }
         }
@@ -36,54 +41,75 @@ void handleClient(int client, int clients[], int *client_sz) {
     fflush(stdout);
 }
 
-int main() {
+int check_client(int client, int clients[], int client_sz)
+{
+    for (int i = 0; i < client_sz; i++)
+    {
+        if (clients[i] == client)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
     char buff[BUF_SIZE];
     struct sockaddr_in caddr;
     struct sockaddr_in saddr = {
         .sin_family = AF_INET,
         .sin_addr.s_addr = htonl(INADDR_ANY),
-        .sin_port = htons(SERVER_PORT)
-    };
+        .sin_port = htons(SERVER_PORT)};
 
     int server = socket(AF_INET, SOCK_STREAM, 0);
     int clients[MAX_CLIENTS];
     int client_sz = 0;
 
-    if (server == -1) {
+    if (server == -1)
+    {
         perror("socket");
         exit(1);
     }
 
-    if (bind(server, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
+    if (bind(server, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+    {
         perror("bind");
         exit(1);
     }
 
-    if (listen(server, 5) == -1) {
+    if (listen(server, 5) == -1)
+    {
         perror("listen");
         exit(1);
     }
 
-    while (1) {
+    while (1)
+    {
         int client, csize = sizeof(caddr);
         client = accept(server, (struct sockaddr *)&caddr, &csize);
-        if (client == -1) {
+        if (client == -1)
+        {
             perror("accept");
             continue;
         }
 
         printf("Client connected: %s\n", inet_ntoa(caddr.sin_addr));
 
-        if (client_sz == MAX_CLIENTS) {
+        if (client_sz == MAX_CLIENTS)
+        {
             puts("Server is full");
             send(client, "Server is full", 14, 0);
             close(client);
             continue;
         }
 
-        clients[client_sz++] = client;
+        if (check_client(client, clients, client_sz))
+        {
+            clients[client_sz++] = client;
+        }
 
-        handleClient(client, clients, &client_sz);
+        handle_client(client, clients, &client_sz);
     }
 
     return 0;
