7951044b19d95b4bca9f2a76f3d669fb33ccd1a0
Lexizz7 <guetron@gmail.com>
2023-10-16 00:12:24 -0300

    multi thread client test

--- client.c
+++ client.c
@@ -6,9 +6,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define SERVER_PORT 5000
 #define BUF_SIZE 4096
+
+int s, bytes;
+
+void *write_messages(void *arg)
+{
+    char buff[BUF_SIZE];
+    int count = 0;
+    while (1)
+    {
+        // Clear the terminal screen
+        printf("\033[H\033[J");
+        printf("Client ID: %d\n", count++);
+
+        // Print the messages
+        bytes = read(s, buff, BUF_SIZE);
+        write(1, buff, bytes);
+
+        // Sleep for a while
+        sleep(1);
+    }
+}
+
+void *get_input(void *arg)
+{
+    char input[BUF_SIZE];
+    while (1)
+    {
+        printf("Type a message: ");
+        fgets(input, BUF_SIZE, stdin);
+
+        // Send the message to the server
+        write(s, input, strlen(input) + 1);
+    }
+}
 
 void fatal(char *string)
 {
@@ -18,13 +53,13 @@ void fatal(char *string)
 
 int main(int argc, char **argv)
 {
-    int c, s, bytes;
-    char buf[BUF_SIZE];
+    int c;
     struct hostent *h;
     struct sockaddr_in channel;
+    pthread_t thread1, thread2;
 
-    if (argc != 3)
-        fatal("Usage: client server-name file-name");
+    if (argc != 2)
+        fatal("Usage: client server-name");
 
     h = gethostbyname(argv[1]);
 
@@ -46,21 +81,12 @@ int main(int argc, char **argv)
     if (c < 0)
         fatal("connect failed");
 
-    write(s, argv[2], strlen(argv[2]) + 1);
+    // Create two threads (one for reading and one for writing)
+    pthread_create(&thread1, NULL, write_messages, NULL);
+    pthread_create(&thread2, NULL, get_input, NULL);
 
-    while (1)
-    {
-        bytes = read(s, buf, BUF_SIZE);
-        printf("%d\n", bytes);
-
-        // if (bytes <= 0)
-        //     exit(0);
-
-        write(1, buf, bytes);
-
-        // Noop loop
-        sleep(1);
-    }
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
     return 0;
 }
