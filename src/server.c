5bab3de976d7c43d0079d68bb5a46956befff60b
Leon Júnio Martins Ferreira <juniohuy2gamer@gmail.com>
2023-10-16 00:24:36 -0300

    Multithread server

--- src/server.c
+++ src/server.c
@@ -6,39 +6,58 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define SERVER_PORT 5000
-#define MAX_CLIENTS 10 // Adjust the maximum number of clients as needed
+#define MAX_CLIENTS 10
 #define BUF_SIZE 4096
+
+int clients[MAX_CLIENTS];
+int client_sz = 0;
 
 void handle_client(int client, int clients[], int *client_sz)
 {
     char buff[BUF_SIZE];
     ssize_t bytesReceived;
 
-    bytesReceived = recv(client, buff, sizeof(buff), 0);
-    if (bytesReceived == -1)
+    while (1)
     {
-        perror("recv");
-        close(client);
-        return;
-    }
-
-    // Broadcast the received message to all clients
-    for (int i = 0; i < *client_sz; i++)
-    {
-        int c = clients[i];
-        if (c != client)
+        bytesReceived = recv(client, buff, sizeof(buff), 0);
+        if (bytesReceived <= 0)
         {
-            if (send(c, buff, bytesReceived, 0) == -1)
+            // Handle client disconnection or error here
+            close(client);
+            // Remove the client from the array (if it exists)
+            for (int i = 0; i < *client_sz; i++)
             {
-                perror("send");
+                if (clients[i] == client)
+                {
+                    // Shift the rest of the clients to fill the gap
+                    for (int j = i; j < *client_sz - 1; j++)
+                        clients[j] = clients[j + 1];
+                    (*client_sz)--;
+                    break;
+                }
+            }
+            return;
+        }
+
+        // Broadcast the received message to all clients
+        for (int i = 0; i < *client_sz; i++)
+        {
+            int c = clients[i];
+            if (c != client)
+            {
+                if (send(c, buff, bytesReceived, 0) == -1)
+                {
+                    perror("send");
+                }
             }
         }
-    }
 
-    puts(buff);
-    fflush(stdout);
+        puts(buff);
+        fflush(stdout);
+    }
 }
 
 int check_client(int client, int clients[], int client_sz)
@@ -53,6 +72,14 @@ int check_client(int client, int clients[], int client_sz)
     return 1;
 }
 
+void *client_thread(void *arg)
+{
+    int client = *(int *)arg;
+    free(arg);
+    handle_client(client, clients, &client_sz);
+    pthread_exit(NULL);
+}
+
 int main()
 {
     char buff[BUF_SIZE];
@@ -63,8 +90,6 @@ int main()
         .sin_port = htons(SERVER_PORT)};
 
     int server = socket(AF_INET, SOCK_STREAM, 0);
-    int clients[MAX_CLIENTS];
-    int client_sz = 0;
 
     if (server == -1)
     {
@@ -86,11 +111,13 @@ int main()
 
     while (1)
     {
-        int client, csize = sizeof(caddr);
-        client = accept(server, (struct sockaddr *)&caddr, &csize);
-        if (client == -1)
+        int csize = sizeof(caddr);
+        int *client = (int *)malloc(sizeof(int));
+        *client = accept(server, (struct sockaddr *)&caddr, &csize);
+        if (*client == -1)
         {
             perror("accept");
+            free(client);
             continue;
         }
 
@@ -99,17 +126,24 @@ int main()
         if (client_sz == MAX_CLIENTS)
         {
             puts("Server is full");
-            send(client, "Server is full", 14, 0);
-            close(client);
+            send(*client, "Server is full", 14, 0);
+            close(*client);
+            free(client);
             continue;
         }
 
-        if (check_client(client, clients, client_sz))
+        if (check_client(*client, clients, client_sz))
         {
-            clients[client_sz++] = client;
+            clients[client_sz++] = *client;
+            pthread_t tid;
+            pthread_create(&tid, NULL, client_thread, client);
+            pthread_detach(tid);
         }
-
-        handle_client(client, clients, &client_sz);
+        else
+        {
+            // If the client is already in the list, free the allocated memory.
+            free(client);
+        }
     }
 
     return 0;
